f2cd4730faa149c4b3c3d4086e65fda1f8327528
thomas <inquister12345678@gmail.com>
2023-12-08 22:36:41 -0800

    Implemented a click event handler on the  element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,15 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        console.log(`User response to the install prompt: ${outcome}`);
+        deferredPrompt = null;
+    }
+    butInstall.style.display = 'none';
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
