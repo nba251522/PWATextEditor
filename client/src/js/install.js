b8950e0cd6e106750058f443b7d502d399b13b3a
thomas <inquister12345678@gmail.com>
2023-12-08 22:37:24 -0800

    added a console log to the app installed event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -20,4 +20,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA was installed');
+});
